a48c163f87296b9c62e82eac28279eff5a35389b
Kamil <31895163+kamil-klasicki@users.noreply.github.com>
2018-12-12 12:48:22 +0000

    Update checkout.php

--- includes/checkout.php
+++ includes/checkout.php
@@ -11,6 +11,6 @@
         });
         </script>
 <div class="clear"></div>
-        <p style="font-style:italic;margin-top:10px;"><?php _e( 'You will be redirected to Divido to complete this finance application when you place your order', 'woothemes' ); ?></p>
+        <p style="font-style:italic;margin-top:10px;"><?php _e( 'You will be redirected  to complete this finance application when you place your order', 'woothemes' ); ?></p>
 </fieldset>
 <?php wp_nonce_field( 'submit-payment-form','submit-payment-form-nonce' ); ?>
