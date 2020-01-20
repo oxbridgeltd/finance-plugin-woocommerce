f63f9f5a71e9dd349ca461e3b863beab040ac1cf
Freddie Line <freddie.line@divido.com>
2020-01-20 11:04:02 +0000

    update variable price

--- js/widget_price_update.js
+++ js/widget_price_update.js
@@ -3,8 +3,9 @@ jQuery(document).on('woocommerce_variation_select_change', function() {
         // When variation is found, grab the display price and update Divido_widget
         jQuery(this).on('found_variation', function(event, variation) {
             var new_price = variation.display_price;
-            var widget = jQuery("[data-"+environment.environment+"-widget]");
-            widget.attr("data-"+environment.environment+"-amount", new_price);
+            var widget = jQuery("[data-calculator-widget]");
+            widget.attr("data-amount", new_price * 100);
+            __widgetInstance.init();
         });
     });
 });
